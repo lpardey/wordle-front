ec29d6da3e695947bf753f46660480ad48aabd9b
lpardey <lpardey@outlook.com>
2023-09-13 17:07:33 +0000

    refactor

--- wordle-front/src/App.js
+++ wordle-front/src/App.js
@@ -1,8 +1,13 @@
 import SignIn from "./SignIn";
+import Typography from "@mui/material/Typography";
 
 function App() {
+  const titleStyle = { display: "flex", justifyContent: "center", margin: "auto", marginTop: 50, color: "#8B38F7", fontWeight: "bold" }
   return (
-    <SignIn />
+    <>
+      <Typography style={titleStyle} component={"h1"} variant="h3">WORDLEMATIC</Typography>
+      <SignIn />
+    </>
   );
 }
 
