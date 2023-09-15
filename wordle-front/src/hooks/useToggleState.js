412d7384f32cfeee5150f60f6d933292e1967de6
lpardey <lpardey@outlook.com>
2023-09-15 00:16:39 +0000

    refactor

--- wordle-front/src/hooks/useToggleState.js
+++
@@ -1,7 +0,0 @@
-import { useState } from "react"
-
-export default function useToggleState(init_value = false) {
-    const [state, setState] = useState(init_value)
-    const toggle = () => { setState(!state) }
-    return [state, toggle]
-}
