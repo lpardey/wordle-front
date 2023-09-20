90dce3832314233c8d99a505377916fd9d4c0162
lpardey <lpardey@outlook.com>
2023-09-20 17:11:41 +0000

    add login and registration pages

--- wordle-front/src/App.js
+++ wordle-front/src/App.js
@@ -1,6 +1,7 @@
 import SharedLayout from "./pages/SharedLayout";
-import Home from "./pages/Home";
 import Game from "./pages/Game";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
 import NotFound from "./pages/NotFound";
 import { Routes, Route } from "react-router-dom";
 
@@ -8,8 +9,9 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Home />} />
-        <Route path="/game" element={<Game />} />
+        <Route index element={<Game />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Registration" element={<Registration />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
