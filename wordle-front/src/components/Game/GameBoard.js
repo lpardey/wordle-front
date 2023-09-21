8a0f767c7857dc8d7b5e5f9f7b56128b014d826f
lpardey <lpardey@outlook.com>
2023-09-21 18:42:07 +0000

    update component

--- wordle-front/src/components/Game/GameBoard.js
+++ wordle-front/src/components/Game/GameBoard.js
@@ -2,10 +2,16 @@ import Guess from "./Guess"
 import Stack from "@mui/material/Stack"
 
 export default function GameBoard({ guesses, getbgC }) {
-    const [guess_1, guess_2, guess_3, guess_4, guess_5, guess_6] = guesses
+    const word_guess_1 = guesses[0].word || ""
+    const status_guess_1 = guesses[0].letters_status || ""
+    const guess_2 = guesses[1] || { word: "", letters_status: "" }
+    const guess_3 = guesses[2] || { word: "", letters_status: "" }
+    const guess_4 = guesses[3] || { word: "", letters_status: "" }
+    const guess_5 = guesses[4] || { word: "", letters_status: "" }
+    const guess_6 = guesses[5] || { word: "", letters_status: "" }
     return (
         <Stack direction={"column"}>
-            <Guess word={guess_1.word} status={guess_1.letters_status} getBackgroundColor={getbgC} />
+            <Guess word={word_guess_1} status={status_guess_1} getBackgroundColor={getbgC} />
             <Guess word={guess_2.word} status={guess_2.letters_status} getBackgroundColor={getbgC} />
             <Guess word={guess_3.word} status={guess_3.letters_status} getBackgroundColor={getbgC} />
             <Guess word={guess_4.word} status={guess_4.letters_status} getBackgroundColor={getbgC} />
--- wordle-front/src/components/Game/Guess.js
+++ wordle-front/src/components/Game/Guess.js
@@ -2,14 +2,33 @@ import Letter from "./Letter"
 import Stack from "@mui/material/Stack"
 
 export default function Guess({ word, status, getBackgroundColor }) {
-    const [status_1, status_2, status_3, status_4, status_5] = status
+    const letter_1 = word[0] || ""
+    const letter_2 = word[1] || ""
+    const letter_3 = word[2] || ""
+    const letter_4 = word[3] || ""
+    const letter_5 = word[4] || ""
+    const status_1 = setStauts(status[0])
+    const status_2 = setStauts(status[1])
+    const status_3 = setStauts(status[2])
+    const status_4 = setStauts(status[3])
+    const status_5 = setStauts(status[4])
     return (
         <Stack direction={"row"}>
-            <Letter letter={word[0]} backgroundColor={getBackgroundColor(status_1)} />
-            <Letter letter={word[1]} backgroundColor={getBackgroundColor(status_2)} />
-            <Letter letter={word[2]} backgroundColor={getBackgroundColor(status_3)} />
-            <Letter letter={word[3]} backgroundColor={getBackgroundColor(status_4)} />
-            <Letter letter={word[4]} backgroundColor={getBackgroundColor(status_5)} />
+            <Letter letter={letter_1} backgroundColor={getBackgroundColor(status_1)} />
+            <Letter letter={letter_2} backgroundColor={getBackgroundColor(status_2)} />
+            <Letter letter={letter_3} backgroundColor={getBackgroundColor(status_3)} />
+            <Letter letter={letter_4} backgroundColor={getBackgroundColor(status_4)} />
+            <Letter letter={letter_5} backgroundColor={getBackgroundColor(status_5)} />
         </Stack>
     )
+}
+
+const setStauts = (value) => {
+    if (value === 0) {
+        return 0
+    } else if (value === 1) {
+        return 1
+    } else {
+        return 2
+    }
 }
