ba23d217fe03300c1ff0e92e5aca51cc2d8048fa
lpardey <lpardey@outlook.com>
2023-09-23 00:07:06 +0000

    add success pop up content component

---
+++ wordle-front/src/components/PopUp/SuccessPopUpContent.js
@@ -0,0 +1,14 @@
+import Typography from "@mui/material/Typography"
+
+export default function SuccessPopUpContent({ successTitle, successMessage }) {
+    return (
+        <>
+            <Typography id="child-modal-title" variant="h6" component="h2">
+                {successTitle}
+            </Typography>
+            <Typography id="child-modal-description" sx={{ mt: 2 }}>
+                {successMessage}
+            </Typography>
+        </>
+    )
+}
