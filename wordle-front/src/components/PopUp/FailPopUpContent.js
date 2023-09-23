5e814d009e808dd4b422bf74d9422497b6bb5a62
lpardey <lpardey@outlook.com>
2023-09-23 00:06:57 +0000

    add fail pop up content component

---
+++ wordle-front/src/components/PopUp/FailPopUpContent.js
@@ -0,0 +1,14 @@
+import Typography from "@mui/material/Typography"
+
+export default function FailPopUpContent({ failTitle, failMessage }) {
+    return (
+        <>
+            <Typography id="child-modal-title" variant="h6" component="h2">
+                {failTitle}
+            </Typography>
+            <Typography id="child-modal-description" sx={{ mt: 2 }}>
+                {failMessage}
+            </Typography>
+        </>
+    )
+}
