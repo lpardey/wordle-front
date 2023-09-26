63c36de6b98e46501f5d7dbed60317fa2ae62048
lpardey <lpardey@outlook.com>
2023-09-26 00:45:04 +0000

    add bold fontweigth prop

--- wordle-front/src/components/PopUp/FailPopUpContent.js
+++ wordle-front/src/components/PopUp/FailPopUpContent.js
@@ -3,7 +3,7 @@ import Typography from "@mui/material/Typography"
 export default function FailPopUpContent({ failTitle, failMessage }) {
     return (
         <>
-            <Typography id="child-modal-title" variant="h6" component="h2">
+            <Typography id="child-modal-title" variant="h6" component="h2" fontWeight={"bold"}>
                 {failTitle}
             </Typography>
             <Typography id="child-modal-description" sx={{ mt: 2 }}>
--- wordle-front/src/components/PopUp/SuccessPopUpContent.js
+++ wordle-front/src/components/PopUp/SuccessPopUpContent.js
@@ -3,7 +3,7 @@ import Typography from "@mui/material/Typography"
 export default function SuccessPopUpContent({ successTitle, successMessage }) {
     return (
         <>
-            <Typography id="child-modal-title" variant="h6" component="h2">
+            <Typography id="child-modal-title" variant="h6" component="h2" fontWeight={"bold"}>
                 {successTitle}
             </Typography>
             <Typography id="child-modal-description" sx={{ mt: 2 }}>
