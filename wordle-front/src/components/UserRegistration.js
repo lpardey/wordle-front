21cb41b6bb450cbfc296d6e1956162a741d4eab6
lpardey <lpardey@outlook.com>
2023-09-26 00:44:19 +0000

    update UserRegistration component

--- wordle-front/src/components/UserRegistration.js
+++ wordle-front/src/components/UserRegistration.js
@@ -10,13 +10,15 @@ import useForm from "../hooks/useForm";
 import WordleClient from '../clients/wordleClient/wordleClient';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import RegistrationPopUp from './Form/RegistrationPopUp';
+import WindowPopUp from './Form/WindowPopUp';
 
 export default function UserRegistration() {
     const [showPassword, togglePassword] = useToggle(false);
     const [inputs, handleChange, resetInputs] = useForm({ username: "", email: "", password: "" })
     const [failMessage, setFailMessage] = useState("")
-    const [openPopUp, togglePopUp] = useToggle(false);
+    const [popUpState, togglePopUp] = useToggle(false);
+    // TODO finish refactor
+    const handleClosePopUp = () => { resetInputs(); togglePopUp(false) }
     const [isSuccess, toggleIsSuccess] = useToggle(false);
     const client = new WordleClient();
     const navigate = useNavigate()
@@ -25,14 +27,15 @@ export default function UserRegistration() {
         const response = await client.createUser(inputs.username, inputs.email, inputs.password);
         if (response !== undefined) {
             setFailMessage(response.detail)
-            togglePopUp(openPopUp)
-            setTimeout(() => { navigate(0) }, 3000)
+            togglePopUp(false)
+            setTimeout(() => { navigate(0) }, 4000)
         } else {
             toggleIsSuccess(isSuccess)
-            togglePopUp(openPopUp)
-            setTimeout(() => { navigate("/") }, 3000)
+            togglePopUp(false)
+            setTimeout(() => { navigate("/") }, 4000)
         }
     }
+    const successMessage = <span>Thank you for joining <span style={{ color: "purple", fontWeight: "bold" }}>Wordlematic</span>. You're being redirected to the game...</span>
     return (
         <>
             <FormTop topText={"Register"} icon={<AppRegistrationIcon />} />
@@ -47,12 +50,12 @@ export default function UserRegistration() {
                 />
                 <FormButton buttonText={"Submit"} />
             </form>
-            {openPopUp ?
-                <RegistrationPopUp
-                    open={openPopUp}
+            {popUpState ?
+                <WindowPopUp
+                    open={popUpState}
                     isSuccess={isSuccess}
-                    succesTitle={"Sucess!"}
-                    succesMessage={"Thank you for joining 'Wordlematic'. You're being redirected to the game..."}
+                    succesTitle={"Success!"}
+                    succesMessage={successMessage}
                     failTitle={"Try again..."}
                     failMessage={`${failMessage}.`}
                 />
