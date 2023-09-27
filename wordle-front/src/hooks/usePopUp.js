0d64b84fb1788b0d260492773e852deb4a18b8ee
lpardey <lpardey@outlook.com>
2023-09-27 22:55:08 +0000

    remove unnecessary component

--- wordle-front/src/hooks/usePopUp.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-
-export default function usePopUp(init_value) {
-    const [state, setState] = useState(init_value)
-    const openPopUp = () => { setState(true) }
-    const closePopUp = () => { setState(false) }
-    return [state, openPopUp, closePopUp]
-}
