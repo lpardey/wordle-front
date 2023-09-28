41fd9e6240b56c736b319b6f944c916197270aaa
lpardey <lpardey@outlook.com>
2023-09-28 17:51:39 +0000

    refactor gamboard component

--- wordle-front/src/components/Game/GameBoard.js
+++ wordle-front/src/components/Game/GameBoard.js
@@ -1,22 +1,28 @@
 import Guess from "./Guess"
 import Stack from "@mui/material/Stack"
 
-export default function GameBoard({ guesses, getbgC }) {
-    const word_guess_1 = guesses[0].word || ""
-    const status_guess_1 = guesses[0].letters_status || ""
-    const guess_2 = guesses[1] || { word: "", letters_status: "" }
-    const guess_3 = guesses[2] || { word: "", letters_status: "" }
-    const guess_4 = guesses[3] || { word: "", letters_status: "" }
-    const guess_5 = guesses[4] || { word: "", letters_status: "" }
-    const guess_6 = guesses[5] || { word: "", letters_status: "" }
+export default function GameBoard({ guesses, maxAttempts }) {
+    const board = getBoard(guesses, maxAttempts)
+    const getBackgroundColor = (number) => number === 0 ? "#61D78C" : number === 1 ? "#F1F483" : "#797979"
     return (
         <Stack direction={"column"}>
-            <Guess word={word_guess_1} status={status_guess_1} getBackgroundColor={getbgC} />
-            <Guess word={guess_2.word} status={guess_2.letters_status} getBackgroundColor={getbgC} />
-            <Guess word={guess_3.word} status={guess_3.letters_status} getBackgroundColor={getbgC} />
-            <Guess word={guess_4.word} status={guess_4.letters_status} getBackgroundColor={getbgC} />
-            <Guess word={guess_5.word} status={guess_5.letters_status} getBackgroundColor={getbgC} />
-            <Guess word={guess_6.word} status={guess_6.letters_status} getBackgroundColor={getbgC} />
+            {board.map(({ word, letters_status }, index) => (
+                <Guess
+                    key={index}
+                    word={word || ""}
+                    status={letters_status || ""}
+                    getBackgroundColor={getBackgroundColor}
+                />
+            ))}
         </Stack>
     )
+}
+
+function getBoard(guesses, maxAttempts) {
+    const guessesAmount = guesses.length
+    if (guessesAmount < maxAttempts) {
+        const toComplete = Array(maxAttempts - guessesAmount).fill({})
+        guesses.push(...toComplete)
+    }
+    return guesses
 }
