c866f06077125a556a53f122017763792f546cd8
lpardey <lpardey@outlook.com>
2023-09-28 17:51:51 +0000

    add on mouse down prop

--- wordle-front/src/components/Game/GameButton.js
+++ wordle-front/src/components/Game/GameButton.js
@@ -1,13 +1,14 @@
 import Button from "@mui/material/Button"
 import { gameButtonSX } from "../../styles/Styles"
 
-export default function GameButton({ handleClick, buttonText }) {
+export default function GameButton({ handleClick, handleMouseDown, buttonText }) {
     return (
         <Button
             type="submit"
             variant="contained"
             size="small"
             onClick={handleClick}
+            onMouseDown={handleMouseDown}
             sx={gameButtonSX}
         >
             {buttonText}
