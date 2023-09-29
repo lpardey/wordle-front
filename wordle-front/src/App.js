ed060741930d3fdc1d89ace382522c811d6c7e5a
lpardey <lpardey@outlook.com>
2023-09-29 22:58:10 +0000

    add dynamic params to the Wordlematic route

--- wordle-front/src/App.js
+++ wordle-front/src/App.js
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Wordlematic />} />
+        <Route path="/:playerId/:gameId" element={<Wordlematic />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
         <Route path="*" element={<PageNotFound />} />
