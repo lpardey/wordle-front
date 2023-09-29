dc756d072e5e9354c1d66fbd19ffbc9783637c9a
lpardey <lpardey@outlook.com>
2023-09-29 22:59:12 +0000

    refactor add create game, get user method

--- wordle-front/src/clients/wordleClient/wordleClient.js
+++ wordle-front/src/clients/wordleClient/wordleClient.js
@@ -1,11 +1,18 @@
 import axios from 'axios'
 import {
+  CreateGameRequest,
   CreateUserRequest,
   CreateUserResponse,
   LoginUserRequest,
   LoginUserResponse,
   TakeAGuessRequest,
-  TakeAGuessResponse
+  TakeAGuessResponse,
+  CreateGameResponse,
+  GetUserRequest,
+  GetUserResponse,
+  FailedCreateUserResponse,
+  FailedLoginUserResponse,
+  FailedGetUserResponse
 } from './schemas'
 
 class WordleClient {
@@ -16,33 +23,56 @@ class WordleClient {
   async createUser(username, email, password) {
     const request = new CreateUserRequest(username, email, password);
     try {
-      await this.client.post("/account/create", request);
+      const rawResponse = await this.client.post("/account/create", request);
+      return new CreateUserResponse(rawResponse.data)
     } catch (error) {
-      const parsed_response = new CreateUserResponse(error.response.data);
-      return parsed_response;
+      return new FailedCreateUserResponse(error.response.data);
+    }
+  }
+
+  async getUser(username) {
+    const request = new GetUserRequest(username);
+    try {
+      const rawResponse = await this.client.get("/account/get", request);
+      return new GetUserResponse(rawResponse.data)
+    } catch (error) {
+      return new FailedGetUserResponse(error.response.data);
     }
   }
 
   async loginUser(username, password) {
     const request = new LoginUserRequest(username, password);
     try {
-      await this.client.post("/account/login", request)
+      const rawResponse = await this.client.post("/account/login", request)
+      return new LoginUserResponse(rawResponse.data)
     } catch (error) {
-      const parsed_response = new LoginUserResponse(error.response.data)
-      return parsed_response
+      return new FailedLoginUserResponse(error.response.data)
     }
   }
 
   async takeAGuess(guess, playerId, gameId) {
     const request = new TakeAGuessRequest(guess)
     try {
-      const raw_response = await this.client.post(`/game/${playerId}/${gameId}/guess`, request)
-      return new TakeAGuessResponse(raw_response.data)
+      const rawResponse = await this.client.post(`/game/${playerId}/${gameId}/guess`, request)
+      return new TakeAGuessResponse(rawResponse.data)
     } catch (error) {
       const message = error.response.data.detail
-      const parsed_response = new TakeAGuessResponse({ status: "ERROR", message: message })
-      return parsed_response
+      return new TakeAGuessResponse({ status: "ERROR", message: message })
     }
+  }
+
+  async createGame(playerId) {
+    const request = new CreateGameRequest(playerId)
+    const rawResponse = await this.client.post(`/game/${playerId}`, request)
+    return new CreateGameResponse(rawResponse.data)
+  }
+
+  async getGameStatus(playerId, gameId) {
+
+  }
+
+  async getPlayerId(username) {
+
   }
 }
 
