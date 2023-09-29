34bc7fa3a39a6ad8516a2584a31d9ef9f82a5f5c
lpardey <lpardey@outlook.com>
2023-09-29 22:58:33 +0000

    add schemas and refactor

--- wordle-front/src/clients/wordleClient/schemas.js
+++ wordle-front/src/clients/wordleClient/schemas.js
@@ -7,8 +7,33 @@ class CreateUserRequest {
 }
 
 class CreateUserResponse {
+    constructor({ player_id }) {
+        this.playerId = player_id;
+    }
+}
+
+class FailedCreateUserResponse {
     constructor({ detail }) {
-        this.detail = detail;
+        this.error = detail
+    }
+}
+
+class GetUserRequest {
+    constructor(username) {
+        this.username = username;
+    }
+}
+
+class GetUserResponse {
+    constructor({ id, username }) {
+        this.gameId = id;
+        this.username = username;
+    }
+}
+
+class FailedGetUserResponse {
+    constructor({ detail }) {
+        this.error = detail
     }
 }
 
@@ -20,8 +45,28 @@ class LoginUserRequest {
 }
 
 class LoginUserResponse {
+    constructor({ access_token, token_type }) {
+        this.accessToken = access_token;
+        this.tokenType = token_type;
+    }
+}
+
+class FailedLoginUserResponse {
     constructor({ detail }) {
-        this.detail = detail;
+        this.error = detail;
+    }
+}
+
+
+class CreateGameRequest {
+    constructor(player_id) {
+        this.playerId = player_id;
+    }
+}
+
+class CreateGameResponse {
+    constructor({ game_id }) {
+        this.gameId = game_id;
     }
 }
 
@@ -32,10 +77,10 @@ class TakeAGuessRequest {
 }
 
 class TakeAGuessResponse {
-    constructor({ guess_result, status, message, guess_letters_status }) {
-        this.guess_result = guess_result;
+    constructor({ status, message, guess_result, guess_letters_status }) {
         this.status = status;
         this.message = message;
+        this.guess_result = guess_result;
         this.letters_status = guess_letters_status;
     }
 
@@ -44,4 +89,18 @@ class TakeAGuessResponse {
     }
 }
 
-export { CreateUserRequest, CreateUserResponse, LoginUserRequest, LoginUserResponse, TakeAGuessRequest, TakeAGuessResponse }
+export {
+    CreateUserRequest,
+    CreateUserResponse,
+    LoginUserRequest,
+    LoginUserResponse,
+    TakeAGuessRequest,
+    TakeAGuessResponse,
+    CreateGameRequest,
+    CreateGameResponse,
+    GetUserRequest,
+    GetUserResponse,
+    FailedCreateUserResponse,
+    FailedGetUserResponse,
+    FailedLoginUserResponse,
+}
