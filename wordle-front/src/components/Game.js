43e4b56ba65ce74ae8d705b43c32738dd3e7b448
lpardey <lpardey@outlook.com>
2023-09-29 23:00:49 +0000

    add useparam hook add initial guessStatus update handleSubmit method refactor snackbar hook

--- wordle-front/src/components/Game.js
+++ wordle-front/src/components/Game.js
@@ -2,26 +2,33 @@ import GameBoard from "./Game/GameBoard";
 import GameInput from "./Game/GameInput";
 import GameButton from "./Game/GameButton";
 import GameSnackbar from "./Game/GameSnackbar";
+import EndGamePopUp from "./Game/EndGamePopUp";
 import WordleClient from "../clients/wordleClient/wordleClient";
 import useToggle from "../hooks/useToggle";
 import { gameFormStyle } from "../styles/Styles";
 import { useEffect, useState } from "react";
-import EndGamePopUp from "./Game/EndGamePopUp";
+import { useParams } from "react-router-dom";
 
 export default function Game() {
     // Get game status info(game word: str, guesses: list[dict[str, list[int]]], game_id) 
     // from api endpoint: http://wordle_back:8000/game/`${game_id}`.
 
+    let {playerId, gameId} = useParams()
+
     const [guesses, setGuesses] = useState([]) // { word: "cloud", letters_status: [0, 2, 2, 2, 1] }
-    const [guessStatus, setGuessStatus] = useState({})
+    const [guessStatus, setGuessStatus] = useState({ status: "", message: "", result: "" })
 
     const [guess, setGuess] = useState("")
     // const [gameStatus, setGameStatus] = useState("WAITING_FOR_GUESS")
     const [maxAttempts, setMaxAttempts] = useState(6)
-    const [openSnackBar, toggleOpenSnackbar] = useToggle(false)
     const [endGamePopUp, toggleEndGamePopUp] = useToggle(false)
 
     const handleChange = (e) => { setGuess(e.target.value) }
+
+    // TODO
+    // hacer un get al back para el game status
+    // guardar el game status en el localStorage
+    // que no muestre el juego si hay partida en el localStorage sino que se cree una nueva
 
     const client = new WordleClient();
     // const [playerId, gameId] = getGameData() // Make a request to get playerId and gameId
@@ -29,29 +36,30 @@ export default function Game() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         setGuess("");
-        const response = await client.takeAGuess(guess, 0, 0)
-        if (response.status !== "ERROR") {
-            guesses.push({ word: guess, letters_status: response.letters_status })
+        const takeAGuessResponse = await client.takeAGuess(guess, playerId, gameId)
+        setGuessStatus({ status: takeAGuessResponse.status, message: takeAGuessResponse.message, result: takeAGuessResponse.guess_result })
+        if (takeAGuessResponse.status !== "ERROR") {
+            guesses.push({ word: guess, letters_status: takeAGuessResponse.letters_status })
             setGuesses(guesses)
-            setGuessStatus({ status: response.status, message: response.message, result: response.guess_result })
         } else {
             // Something unexpected happened
         }
     }
     // Snackbar 
     const [snackPack, setSnackPack] = useState([]);
+    const [openSnackBar, setOpenSnackbar] = useState(false)
     const [messageInfo, setMessageInfo] = useState(undefined);
     useEffect(() => {
         if (snackPack.length && !messageInfo) {
             // Set a new snack when we don't have an active one
             setMessageInfo({ ...snackPack[0] });
             setSnackPack((prev) => prev.slice(1));
-            toggleOpenSnackbar();
+            setOpenSnackbar(true);
         } else if (snackPack.length && messageInfo && openSnackBar) {
             // Close an active snack when a new one is added
-            toggleOpenSnackbar();
+            setOpenSnackbar(false);
         }
-    }, [snackPack, messageInfo, openSnackBar, toggleOpenSnackbar]);
+    }, [snackPack, messageInfo, openSnackBar]);
 
     const handleClick = (message) => () => {
         setSnackPack((prev) => [...prev, { message, key: new Date().getTime() }]);
@@ -60,12 +68,13 @@ export default function Game() {
         if (reason === 'clickaway') {
             return;
         }
-        toggleOpenSnackbar()
+        setOpenSnackbar(false);
     };
-    const gameSnackbarMessage = !guessStatus.status ? guessStatus.message : `Attempts left: ${maxAttempts - guesses.length}`
+    const gameSnackbarMessage = guessStatus.status === "ERROR" ? guessStatus.message : `Attempts left: ${maxAttempts - guesses.length}`
     const isOver = (guessStatus.status === "OK" && guessStatus.result === "GUESSED") || (guessStatus.status === "ERROR" && guesses.length === maxAttempts)
     return (
         <>
+            {console.log(guessStatus)}
             <GameBoard guesses={guesses} maxAttempts={maxAttempts} />
             <form onSubmit={handleSubmit} style={gameFormStyle}>
                 <GameInput guess={guess} handleChange={handleChange} />
