b94510f87188606485bb3ee68295237724cc6691
lpardey <lpardey@outlook.com>
2023-11-23 21:45:52 +0000

    update component logic in reference to game result

--- wordle-front/src/components/Game/EndGamePopUp.js
+++ wordle-front/src/components/Game/EndGamePopUp.js
@@ -3,8 +3,8 @@ import Box from "@mui/material/Box";
 import PopUpContent from "../PopUp/PopUpContent";
 import { boxPopUpSX } from "../../styles/Styles";
 
-export default function EndGamePopUp({ open, handleClose, guessStatusResult, winnerTitle, winnerMessage, gameOverTitle, gameOverMessage }) {
-    const [title, message] = getContent(guessStatusResult, winnerTitle, winnerMessage, gameOverTitle, gameOverMessage)
+export default function EndGamePopUp({ open, handleClose, gameResult, winnerTitle, winnerMessage, gameOverTitle, gameOverMessage }) {
+    const [title, message] = getContent(gameResult, winnerTitle, winnerMessage, gameOverTitle, gameOverMessage)
     return (
         <Modal
             open={open}
@@ -19,10 +19,10 @@ export default function EndGamePopUp({ open, handleClose, guessStatusResult, win
     )
 }
 
-function getContent(guessStatusResult, winnerTitle, winnerMessage, gameOverTitle, gameOverMessage) {
+function getContent(gameResult, winnerTitle, winnerMessage, gameOverTitle, gameOverMessage) {
     let title;
     let message;
-    if (guessStatusResult === "GUESSED") {
+    if (gameResult === "VICTORY") {
         title = winnerTitle
         message = winnerMessage
     } else {
