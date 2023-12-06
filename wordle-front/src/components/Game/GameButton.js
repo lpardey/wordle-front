3cc11479e39c396d1c7d5ecdb9ce540eebb49ecc
lpardey <lpardey@outlook.com>
2023-12-06 18:25:53 +0000

    remove unnecessary behaivor

--- wordle-front/src/components/Game/GameButton.js
+++ wordle-front/src/components/Game/GameButton.js
@@ -1,13 +1,12 @@
 import Button from "@mui/material/Button"
 import { gameButtonSX } from "../../styles/Styles"
 
-export default function GameButton({ buttonText, handleClick, handleMouseDown, handleDisabled }) {
+export default function GameButton({ buttonText, handleMouseDown, handleDisabled }) {
     return (
         <Button
             type="submit"
             variant="contained"
             size="small"
-            onClick={handleClick}
             onMouseDown={handleMouseDown}
             sx={gameButtonSX}
             disabled={handleDisabled}
